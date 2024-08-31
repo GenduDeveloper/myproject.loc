d109d2e78d187b27fa71baeb6f7594a315c39da3
Ilya Avkhimenko <avximenko@inbox.ru>
2024-08-31 16:06:38 +0800

    added .htaccess

--- www/index.php
+++ www/index.php
@@ -4,3 +4,4 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $author = new \MyProject\Models\Users\User('Илья');
 $article = new \MyProject\Models\Articles\Article('Заголовок', 'Текст', $author);
+var_dump($article);
