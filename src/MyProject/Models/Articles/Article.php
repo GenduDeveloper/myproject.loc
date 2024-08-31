9902112eee5de637333ee0585c34b28334dc13b8
Ilya Avkhimenko <avximenko@inbox.ru>
2024-08-31 15:41:06 +0800

    TASK-1: added namespace and autoload

---
+++ src/MyProject/Models/Articles/Article.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MyProject\Models\Articles;
+use MyProject\Models\Users\User;
+
+class Article
+{
+    private string $title;
+    private string $text;
+    private User $author;
+    public function __construct(string $title, string $text, User $author)
+    {
+        $this->title = $title;
+        $this->text = $text;
+        $this->author = $author;
+    }
+
+    public function getTitle(): string
+    {
+       return $this->title;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+}
