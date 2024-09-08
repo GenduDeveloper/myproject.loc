9a13b3992622e41857b20164c3bbb37224ab0777
Ilya Avkhimenko <avximenko@inbox.ru>
2024-09-08 18:22:36 +0800

    TASK-10: updating and inserting into the database via ActiveRecord

--- src/MyProject/Controllers/ArticlesController.php
+++ src/MyProject/Controllers/ArticlesController.php
@@ -25,4 +25,28 @@ class ArticlesController
 
         $this->view->renderHtml('articles/view.php', ['article' => $article]);
     }
+
+    public function edit(int $articleId): void
+    {
+        $article = Article::getById($articleId);
+
+        if ($article === null) {
+            $this->view->renderHtml('errors/404.php', [], 404);
+            return;
+        }
+
+        $article->setName('Новое имя статьи');
+        $article->setText('Новый текст статьи');
+        $article->save();
+    }
+
+    public function create(): void
+    {
+        $article = new Article();
+        $article->setName('Новое имя 1');
+        $article->setText('Новый текст 2');
+        $article->setAuthorId(1);
+        $article->setCreatedAt(date('Y-m-d H:i:s'));
+        $article->save();
+    }
 }
