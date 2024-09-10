a708e698a191cbe0d449139621aa196697f37e0a
Ilya Avkhimenko <avximenko@inbox.ru>
2024-09-10 11:13:43 +0800

    TASK-13: working with Exceptions

---
+++ templates/errors/500.php
@@ -0,0 +1,8 @@
+<?php include_once __DIR__ . '/../header.php' ?>
+
+<div class="container">
+    <h1 class="section-heading" style="text-align: center">Хьюстон, у нас проблемы</h1><br><br>
+    <p><?= $error ?></p>
+</div>
+
+<?php include_once __DIR__ . '/../footer.php' ?>
